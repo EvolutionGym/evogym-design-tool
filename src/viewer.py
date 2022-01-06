123e8873fb2da4a66b038904080d2d7f9ab3a698
Yunsheng Tian <yunsheng@csail.mit.edu>
2022-01-06 01:22:20 -0500

    Fix conda env and OpenGL import bugs on macos

--- src/viewer.py
+++ src/viewer.py
@@ -1,3 +1,18 @@
+try:
+    import OpenGL
+    try:
+        import OpenGL.GL   # this fails in <=2020 versions of Python on OS X 11.x
+    except ImportError:
+        from ctypes import util
+        orig_util_find_library = util.find_library
+        def new_util_find_library( name ):
+            res = orig_util_find_library( name )
+            if res: return res
+            return '/System/Library/Frameworks/'+name+'.framework/'+name
+        util.find_library = new_util_find_library
+except ImportError:
+    pass
+
 import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
